39d2c2314a9e2846022f40c7dbc93261f806a729
Damyan Momchev <dastmo@gmail.com>
2020-11-05 21:12:11 +0200

    Dustbin implemented
     - Implemented the first dustbin functions;

---
+++ Assets/Scripts/Dustbin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dustbin : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer outsideSprite;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debris debris = collision.GetComponent<Debris>();
+
+        if (debris)
+        {
+            Debug.Log("Debris caught!");
+            Destroy(collision.gameObject);
+            return;
+        }
+
+        PlayerControls player = collision.GetComponent<PlayerControls>();
+
+        if (player)
+        {
+            HideOutsideSprite();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerControls player = collision.GetComponent<PlayerControls>();
+
+        if (player)
+        {
+            ShowOutsideSprite();
+        }
+    }
+
+    private void HideOutsideSprite()
+    {
+        if (outsideSprite == null) outsideSprite = GetComponent<SpriteRenderer>();
+
+        outsideSprite.color = new Color(.5f, .5f, .5f, 0.25f);
+    }
+
+    private void ShowOutsideSprite()
+    {
+        if (outsideSprite == null) outsideSprite = GetComponent<SpriteRenderer>();
+
+        outsideSprite.color = Color.white;
+    }
+}
